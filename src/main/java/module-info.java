496bd0239aa9db305cbc9ff0ff502c6f89310f28
Luis Daniel Mesa Velásquez <luism@OMEGA.worldshack.org>
2021-01-21 22:51:19 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module io.luismesa.javafx.test {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens io.luismesa.javafx.test to javafx.fxml;
+    exports io.luismesa.javafx.test;
+}
+*/
